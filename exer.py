40f6d67bd579ca10cd45b0418cb931c507a62db9
lowyiheng <yiheng.low@my.jcu.edu.my>
2023-07-30 02:27:47 +0800

    hi

--- exer.py
+++ exer.py
@@ -1,23 +1,23 @@
-out_file = open("name.txt", "w")
+out_file = open("CP1404/prac_03/name.txt", "w")
 name = input("What is your name? ")
 print(name, file=out_file)
 out_file.close()
 
 # 2
-file = open("name.txt", "r")
+file = open("CP1404/prac_03/name.txt", "r")
 name = file.read().strip()
 file.close()
 print("Your name is", name)
 
 # 3
-file = open("numbers.txt", "r")
+file = open("CP1404/prac_03/numbers.txt", "r")
 first_number = int(file.readline())
 second_number = int(file.readline())
 print(first_number + second_number)
 
 # 4
 total = 0
-file = open("numbers.txt", "r")
+file = open("CP1404/prac_03/numbers.txt", "r")
 for line in file:
     number = int(line)
     total += number
